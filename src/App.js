1091d07f27b68b32ed36843033fea387f865e12b
Rasika Kariyawasam <rasika.kariyawasam@attuneconsulting.com>
2019-05-23 17:04:50 +0530

    rk : add new product

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState } from 'react'
 import axios from "axios";
 import ProductTable from './tables/ProductTable'
+import AddProductForm from './forms/AddProductForm'
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddProductForm />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
